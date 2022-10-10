cd1ca8d7d4d561fca15edfaec1e379b81df088ed
Ivan Barinskiy <ivan.barinskiy@genomicsplc.com>
2022-10-10 13:22:36 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import beeMovie from './text.jpg';
 import './App.css';
+import Tesseract from 'tesseract.js';
 
 function App() {
+  //Tesseract.recognize(beeMovie, 'eng').then((text) => console.log(text.data.lines[0]))
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <img src={beeMovie} />
     </div>
   );
 }
